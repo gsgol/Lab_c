60026d309db84c0bc3f8fb858759e49ea74052c3
gsgol <77744037+gsgol@users.noreply.github.com>
2021-12-29 22:45:02 +0300

    Create Lab.cpp

---
+++ Lab.cpp
@@ -0,0 +1,60 @@
+// Lab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//Задача 8
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool funct(int a, int b)
+{
+    return a > b;
+}
+int main()
+{
+    int a[8] = { -3,8,6,-9,-8,5,-10,23 };
+    sort(a, a + 8, funct);
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << a[i] << ' ';
+    }
+}
+// Точки с координатами
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+
+double distance(int x1, int y1, int x2, int y2)
+{
+	return sqrt(pow((x2 - x1),2) + pow((y2  - y1),2));
+}
+int main()
+{
+	int x[6] = { 2,3,4,5,6,7 };
+	int y[6] = { 1,8,7,9,6,2 };
+	double dist[6][6];
+	double min = distance(x[1], y[1], x[2], y[2]), max = -1;
+	int min_1_id = 1, min_2_id = 2, max_id = -1;
+	for (int i = 0; i < 6; ++i)
+	{
+		for (int j = 0; j < 6; ++j)
+		{
+			dist[i][j] = distance(x[i], y[i], x[j], y[j]);
+			if (dist[i][j] < min && i != j)
+			{
+				min = dist[i][j];
+				min_1_id = i;
+				min_2_id = j;
+			}
+		}
+	}
+	for (int i = 0; i < 6; ++i)
+	{
+		if (dist[min_1_id][i] + dist[min_2_id][i] > max)
+		{
+			max = dist[min_1_id][i] + dist[min_2_id][i];
+			max_id = i;
+		}
+
+	}
+	cout << min << ' ' << min_1_id << ' ' << min_2_id << ' ' << max << ' ' << max_id;
+}
